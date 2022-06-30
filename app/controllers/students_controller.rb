02306698dd1e727d845cee335f303cfe84ef744e
Brigham <brig.johnson@gmail.com>
2022-06-30 12:23:13 -0400

    commit tree 538feabd0376eea6d22492a71babcf7c8ecfdcf6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    #byebug
+    if (params.has_key?(:name)) then
+      n = params[:name].downcase
+      #students = Student.find_by("LOWER(first_name)= ? OR LOWER(last_name)= ?", n, n)
+      #students = Student.find("LOWER(first_name)= ? OR LOWER(last_name)= ?", n, n)
+      students = Student.where("LOWER(first_name)= ? OR LOWER(last_name)= ?", n, n)
+      render json: students
+    else
+      students = Student.all
+      render json: students
+    end
+  end
+
+  def show
+    #byebug
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/students', to: 'students#index'
+  # 'returns students whose first or last name matches the query'
+  # get '/students?name=kirby'
+
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get '/students/:id', to: 'students#show'
 end
